2ee6080d101995a6a7ca3e14c71989a8dbba819c
haqiqi <rzkhqq@gmail.com>
2018-11-03 11:59:27 +0100

    Load from gltf model funcionality

--- formitas/src/components/visualizer/Scene.js
+++ formitas/src/components/visualizer/Scene.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import * as THREE from "three";
 import OrbitControls from "orbit-controls-es6";
+import GLTFLoader from 'three-gltf-loader';
 
 class Scene extends Component {
   render() {
@@ -17,52 +18,51 @@ class Scene extends Component {
 
   componentDidMount() {
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x222222);
+    scene.background = new THREE.Color(0xffcc80);
 
+    // model
+    var loader = new GLTFLoader()
+    loader.load('models/DamagedHelmet/glTF/DamagedHelmet.gltf', function (gltf) {
+      scene.add(gltf.scene);
+    });
+    
+    // renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    //renderer.setClearColor(0xffffff, 1.0);
+    renderer.setClearColor(0xffffff, 1.0);
 
-    // const dom = document.getElementById("three");
-    // dom.appendChild(renderer.domElement);
-
+    // camera
     const camera = new THREE.PerspectiveCamera(
       50,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    camera.position.z = 15;
+    camera.position.z = 5;
 
+    // controls
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enabled = true;
     controls.maxDistance = 1500;
     controls.minDistance = 0;
-
-    const geometry = new THREE.SphereGeometry();
-    const material = new THREE.MeshNormalMaterial({
-      color: 0xff0000
-    });
-
-    const globe = new THREE.Mesh(geometry, material);
-    scene.add(globe);
-
+    
+    //light
     const light_p = new THREE.PointLight(0xffffff);
     light_p.position.set(10, 10, 10);
     scene.add(light_p);
 
-    // const light_a = new THREE.AmbientLight(0x333333);
-    // scene.add(light_a);
+    const light_a = new THREE.AmbientLight(0x333333);
+    scene.add(light_a);
+
+    // animate
     const animate = () => {
       requestAnimationFrame(animate);
-      // const zoom = controls.object.position.distanceTo(controls.target);
       renderer.render(scene, camera);
     };
     this.container.appendChild(renderer.domElement);
 
     animate();
 
-    this.globe = globe;
   }
 
   shouldComponentUpdate() {
--- formitas/src/index.js
+++ formitas/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Visualizer from './components/visualizer/Visualizer';
 import * as serviceWorker from './serviceWorker';
 
