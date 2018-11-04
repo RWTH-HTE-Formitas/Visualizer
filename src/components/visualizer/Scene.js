6c6d9d528898f1d1ce3a5e39a940a5fd08e3e88c
haqiqi <rzkhqq@gmail.com>
2018-11-04 21:50:12 +0100

    auto adjust camera and position based on object

--- src/components/visualizer/Scene.js
+++ src/components/visualizer/Scene.js
@@ -20,40 +20,58 @@ class Scene extends Component {
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffcc80);
 
-    // model
-    var loader = new GLTFLoader()
-    loader.load('models/DamagedHelmet/glTF/DamagedHelmet.gltf', function (gltf) {
-      scene.add(gltf.scene);
-    });
-    
-    // renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xffffff, 1.0);
-
     // camera
     const camera = new THREE.PerspectiveCamera(
       50,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      2000
     );
-    camera.position.z = 5;
+    camera.position.z = 100;
+
+    // renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xffffff, 1.0);
 
     // controls
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enabled = true;
     controls.maxDistance = 1500;
     controls.minDistance = 0;
-    
+
     //light
-    const light_p = new THREE.PointLight(0xffffff);
-    light_p.position.set(10, 10, 10);
+    let light_p = new THREE.HemisphereLight(0xbbbbff, 0x444422);
+    light_p.position.set(1000, 1000, 1000);
     scene.add(light_p);
 
-    const light_a = new THREE.AmbientLight(0x333333);
-    scene.add(light_a);
+    light_p = new THREE.HemisphereLight(0xbbbbff, 0x444422);
+    light_p.position.set(-1000, 1000, -1000);
+    scene.add(light_p);
 
+    // model
+    var loader = new GLTFLoader();
+    loader.load('models/DamagedHelmet/glTF/DamagedHelmet.gltf', function (gltf) {
+
+      var box = new THREE.Box3().setFromObject(gltf.scene.children[0]);
+
+      gltf.scene.children[0].position.x = -1 * box.getCenter().x;
+      gltf.scene.children[0].position.y = -1 * box.getCenter().y;
+      gltf.scene.children[0].position.z = -1 * box.getCenter().z;
+      
+      scene.add(gltf.scene);
+
+      var size = Math.max(box.getSize().x, box.getSize().y, box.getSize().z);
+      var gridHelper = new THREE.GridHelper(size, size/5);
+      scene.add(gridHelper);
+
+      var gridHelperY = new THREE.GridHelper(size, size / 5);
+      gridHelperY.rotation.x = Math.PI / 2;
+      scene.add(gridHelperY);
+
+      camera.position.z = box.getSize().z*1.5;
+    });
+    
     // animate
     const animate = () => {
       requestAnimationFrame(animate);
