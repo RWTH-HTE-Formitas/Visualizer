c7c18f2c7616cb9f73fb0704dd5f3f2fb5aabd44
vschwaiger <verena.schwaiger@rwth-aachen.de>
2018-11-08 15:36:42 +0100

    Added exploration functionalities for keyboard: zooming, camera movement on axes and object selection/transparency toggle

--- src/components/visualizer/Scene.js
+++ src/components/visualizer/Scene.js
@@ -71,7 +71,109 @@ class Scene extends Component {
 
       camera.position.z = box.getSize().z*1.5;
     });
-    
+
+    var projector = new THREE.Projector();
+    window.addEventListener( 'mousedown', onMouseDown, false );
+    window.addEventListener('keypress', onKeyPress, false);
+
+    // Select mode: 0 = Transparency on, 1 = Object selection on/transparency off
+    var objectSelection = 1;
+
+  // Toggle modes via key press
+  function onKeyPress(event){
+    var keyPressed = event.which;
+    var delta = 50; 
+    //var rotation = 0.01;
+    var zoom = 1.0;
+
+    switch(keyPressed){
+      case 122: // z: Object transparency off
+        objectSelection = 1;
+        break;
+      case 116:// t: Object transparency on
+        objectSelection = 0;
+        break;
+      case 37: //left arrow: move camera
+        camera.position.x = camera.position.x - delta;
+        break;
+      case 38: //up arrow: move camera
+        camera.position.y = camera.position.y + delta;
+        break;
+      case 39: //right arrow: move camera
+        camera.position.x = camera.position.x + delta;
+        break;
+      case 40: //down arrow: move camera
+        camera.position.y = camera.position.y - delta;
+        break;
+      case 120: // x : zoom out
+        camera.position.z = camera.position.z + zoom;
+        break;
+      case 121: // y : zoom in
+        camera.position.z = camera.position.z - zoom;        
+        break;
+      case 115: // s : rotate down
+        break;
+      case 119: // w : rotate up
+        break;
+      case 97: //a: rotate left
+        break;
+      case 100: //d: rotate right
+        break;
+
+      default:
+        break;
+    }
+
+  }
+
+  // Either make objects transparent/opaque or select and color them on mouse click event depending on selected mode
+  function onMouseDown(event){
+
+    var mouseVector = new THREE.Vector3(
+      ( event.clientX / window.innerWidth ) * 2 - 1, 
+      -( event.clientY / window.innerHeight ) * 2 + 1, 
+      0.5);
+    projector.unprojectVector(mouseVector, camera);
+    var raycaster = new THREE.Raycaster(camera.position, mouseVector.sub(camera.position).normalize());
+    var intersects = raycaster.intersectObjects( scene.children, true );
+
+    if(objectSelection === 0){    
+      
+
+      if (intersects.length > 0) {
+        intersects[0].object.material.transparent = true;
+        if (intersects[0].object.material.opacity < 1) {
+            intersects[0].object.material.opacity = 1;
+        } else {
+            intersects[0].object.material.opacity = 0.3;
+        }
+
+      }
+    }
+
+    else if(objectSelection === 1){
+      /*if (intersects.length > 0) {
+        intersects[0].object.material.transparent = true;
+       if(intersects[0].object.material.opacity === 1){
+          for ( var i = 0; i < intersects.length; i++ ) {
+            intersects[i].object.material.color.setHex(0xffff00);
+            intersects[0].object.material.opacity = 0.9;
+          }
+        }
+        else {
+          for ( var i = 0; i < intersects.length; i++ ) {
+            intersects[i].object.material.color.setHex(0xffffff);
+            intersects[0].object.material.opacity = 1;
+          }
+        }
+      }*/
+
+
+  }
+}
+
+      
+
     // animate
     const animate = () => {
       requestAnimationFrame(animate);
