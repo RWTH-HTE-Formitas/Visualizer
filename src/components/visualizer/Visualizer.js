8ce6e86b140ad7360a981d40d340f6addd605859
haqiqi <rzkhqq@gmail.com>
2018-11-21 13:39:39 +0100

    object detail window with dummy data implemented

--- src/components/visualizer/Visualizer.js
+++ src/components/visualizer/Visualizer.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import Scene from "./Scene";
+import FloatingWindow from "../window/Window";
 
 const styles = {
     width: "100vw",
-    height: "100vh"
+    height: "100vh",
+    overflow: "hidden",
 };
 
 class Visualizer extends Component {
 
     modelLocation = "https://raw.githubusercontent.com/RWTH-HTE-Formitas/Visualizer/tmp/public/model.gltf";
 
+    dt = {
+        name: 'Horizontal Pipe 101',
+        annotation: 'Direction is not quite right'
+    }
+
     render() {
         return (
             <div style={styles}>
                 <Scene modelLocation={this.modelLocation} />
-                <div style={{ zIndex: 1, position: "absolute" }}></div>
+                <FloatingWindow data={this.dt}/>
             </div>
         );
     }
