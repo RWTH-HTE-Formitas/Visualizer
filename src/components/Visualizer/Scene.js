a72e36a2c51422d2d84a0b94e25b9dca7594975d
Arne Groskurth <arne.groskurth@gears9.de>
2019-01-12 03:35:23 +0100

    Refactoring

--- src/components/Visualizer/Scene.js
+++ src/components/Visualizer/Scene.js
@@ -9,6 +9,20 @@ const height = 400;
 
 class Scene extends Component {
 
+  constructor(props) {
+
+    super(props);
+
+    this.renderer = null;
+    this.scene = null;
+    this.camera = null;
+    this.controls = null;
+    this.loader = null;
+
+    this.objectSelection = 1; // Select mode: 0 = Transparency on, 1 = Object selection on/transparency off
+    this.clickedObjectId = null;
+  }
+
   render() {
 
     return (
@@ -16,61 +30,47 @@ class Scene extends Component {
     );
   }
 
-  clickedObjectId = null;
-  scene = null;
-  camera = null;
-
   componentDidMount() {
 
-    var self = this;
-    var scene, camera, renderer, controls, loader;
-    var objectSelection = 1; // Select mode: 0 = Transparency on, 1 = Object selection on/transparency off
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xffffff);
 
-    init();
-    animate();
+    this.camera = new THREE.PerspectiveCamera(
+      50,
+      width/height,
+      0.1,
+      2000
+    );
 
-    function init() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(width, height);
+    this.renderer.setClearColor(0xffffff, 1.0);
+    this.renderer.domElement.addEventListener("click", this.onClick, false);
+    this.renderer.domElement.addEventListener("keydown", this.onKeyDown);
+    this.renderer.domElement.setAttribute("tabindex", -1); // required for canvas-element to be focusable which is required for handling keyboard events
+    this.renderer.domElement.addEventListener("click", function(event) { event.target.focus(); });
 
-      scene = new THREE.Scene();
-      scene.background = new THREE.Color(0xffffff);
-
-      camera = new THREE.PerspectiveCamera(
-        50,
-        width/height,
-        0.1,
-        2000
-      );
-
-      self.scene = scene;
-      self.camera = camera;
-
-      renderer = new THREE.WebGLRenderer({ antialias: true });
-      renderer.setSize(width, height);
-      renderer.setClearColor(0xffffff, 1.0);
-      renderer.domElement.addEventListener('click', onClick, false);
-      renderer.domElement.addEventListener("keydown", onKeyDown);
-      renderer.domElement.setAttribute("tabindex", -1); // required for canvas-element to be focusable which is required for handling keyboard events
-      renderer.domElement.addEventListener("click", function(event) { event.target.focus(); });
-
-      controls = new OrbitControls(camera, renderer.domElement);
-      controls.mouseButtons = {
-          ORBIT: THREE.MOUSE.RIGHT,
-          ZOOM: THREE.MOUSE.MIDDLE,
-          PAN: THREE.MOUSE.LEFT
-      };
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.mouseButtons = {
+      ORBIT: THREE.MOUSE.RIGHT,
+      ZOOM: THREE.MOUSE.MIDDLE,
+      PAN: THREE.MOUSE.LEFT
+    };
 
 
-      let lightA = new THREE.HemisphereLight(0xbbbbff, 0x444422);
-      lightA.position.set(1000, 1000, 1000);
-      scene.add(lightA);
+    let lightA = new THREE.HemisphereLight(0xbbbbff, 0x444422);
+    lightA.position.set(1000, 1000, 1000);
+    this.scene.add(lightA);
 
-      let lightB = new THREE.HemisphereLight(0xbbbbff, 0x444422);
-      lightB.position.set(-1000, 1000, -1000);
-      scene.add(lightB);
-      
+    let lightB = new THREE.HemisphereLight(0xbbbbff, 0x444422);
+    lightB.position.set(-1000, 1000, -1000);
+    this.scene.add(lightB);
 
-      loader = new GLTFLoader();
-      loader.load(self.props.modelLocation, function (gltf) {
+
+    this.loader = new GLTFLoader();
+    this.loader.load(this.props.modelLocation, function(self) {
+
+      return function (gltf) {
 
         var box = new THREE.Box3().setFromObject(gltf.scene.children[0]);
         var point = new THREE.Vector3();
@@ -79,121 +79,130 @@ class Scene extends Component {
         gltf.scene.children[0].position.y = -1 * box.getCenter(point).y;
         gltf.scene.children[0].position.z = -1 * box.getCenter(point).z;
 
-        scene.add(gltf.scene.children[0]);
+        self.scene.add(gltf.scene.children[0]);
 
-        camera.position.z = box.getSize(point).z * 1.5;
+        self.camera.position.z = box.getSize(point).z * 1.5;
+      };
+
+    }(this));
+
+    this.container.appendChild(this.renderer.domElement);
+
+
+    const animate = function(self) {
+
+      return function() {
+
+        requestAnimationFrame(animate);
+
+        self.controls.update();
+        self.renderer.render(self.scene, self.camera);
+      };
+
+    }(this);
+
+    animate();
+  }
+
+  onKeyDown(event) {
+
+    const delta = 50;
+    const theta = 0.01;
+    const zoom = 1.0;
+
+    switch (event.key) {
+
+      case "t":
+
+        // Toggle object transparency
+        this.objectSelection = (this.objectSelection === 1) ? 0 : 1;
+
+        break;
+
+      case "ArrowLeft": this.camera.position.x = this.camera.position.x - delta; break;
+      case "ArrowUp": this.camera.position.y = this.camera.position.y + delta; break;
+      case "ArrowRight": this.camera.position.x = this.camera.position.x + delta; break;
+      case "ArrowDown": this.camera.position.y = this.camera.position.y - delta; break;
+      case "x": this.camera.position.z = this.camera.position.z + zoom; break;
+      case "y": this.camera.position.z = this.camera.position.z - zoom; break;
+      case "s": this.camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), (-theta)); break;
+      case "w": this.camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), theta); break;
+      case "a": this.camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), theta); break;
+      case "d": this.camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), (-theta)); break;
+
+      default: break;
+    }
+  }
+
+  // Either make objects transparent/opaque or select and color them on mouse click event depending on selected mode
+  onclick(event) {
+
+    const mouseVector = new THREE.Vector3(
+      (event.offsetX / width) * 2 - 1,
+      -(event.offsetY / height) * 2 + 1,
+      0.5
+    );
+    mouseVector.unproject(this.camera);
+
+    const raycaster = new THREE.Raycaster(this.camera.position, mouseVector.sub(this.camera.position).normalize());
+    const intersects = raycaster.intersectObjects(this.scene.children, true);
+
+    if (intersects.length > 0) {
+
+      this.props.callBack({
+        id: intersects[0].object.id,
+        name: intersects[0].object.name
       });
 
-      self.container.appendChild(renderer.domElement);
-    }
+      // highlighting
+      const obj = this.scene.getObjectById(intersects[0].object.id, true);
 
-    function animate() {
+      if (this.clickedObjectId !== obj.id) {
 
-      requestAnimationFrame(animate);
+        obj.currentHex = obj.material.emissive.getHex();
+        obj.material.emissive.setHex(0xffff00);
 
-      renderer.render(scene, camera);
-    }
-
-    function onKeyDown(event) {console.log(event);
-
-      var delta = 50;
-      var theta = 0.01;
-      var zoom = 1.0;
-
-      switch (event.key) {
-
-        case "t":
-
-          // Toggle object transparency
-          objectSelection = (objectSelection === 1) ? 0 : 1;
-
-          break;
-
-        case "ArrowLeft": camera.position.x = camera.position.x - delta; break;
-        case "ArrowUp": camera.position.y = camera.position.y + delta; break;
-        case "ArrowRight": camera.position.x = camera.position.x + delta; break;
-        case "ArrowDown": camera.position.y = camera.position.y - delta; break;
-        case "x": camera.position.z = camera.position.z + zoom; break;
-        case "y": camera.position.z = camera.position.z - zoom; break;
-        case "s": camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), (-theta)); break;
-        case "w": camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), theta); break;
-        case "a": camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), theta); break;
-        case "d": camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), (-theta)); break;
-
-        default: break;
-      }
-    }
-
-    // Either make objects transparent/opaque or select and color them on mouse click event depending on selected mode
-    function onClick(event) {
-
-      var mouseVector = new THREE.Vector3(
-        (event.offsetX / width) * 2 - 1,
-        -(event.offsetY / height) * 2 + 1,
-        0.5
-      );
-      mouseVector.unproject(camera);
-
-      var raycaster = new THREE.Raycaster(camera.position, mouseVector.sub(camera.position).normalize());
-      var intersects = raycaster.intersectObjects(scene.children, true);
-
-      if (intersects.length > 0) {
-
-        self.props.callBack({
-          id: intersects[0].object.id,
-          name: intersects[0].object.name
-        });
-
-        // highlighting
-        const obj = scene.getObjectById(intersects[0].object.id, true);
-
-        if (self.clickedObjectId !== obj.id) {
-
-          obj.currentHex = obj.material.emissive.getHex();
-          obj.material.emissive.setHex(0xffff00);
-          
-          // reset previous
-          if (self.clickedObjectId != null) {
-
-            const exist = scene.getObjectById(self.clickedObjectId, true);
-
-            exist.material.emissive.setHex(exist.currentHex);
-          }
-
-          self.clickedObjectId = obj.id;
-        }
-
-        if (objectSelection === 0) {
-
-          intersects[0].object.material.transparent = true;
-          if (intersects[0].object.material.opacity < 1) {
-
-            intersects[0].object.material.opacity = 1;
-          }
-          else {
-
-            intersects[0].object.material.opacity = 0.3;
-          }
-        }
-        else if (objectSelection === 1) {
-
-          /* Object is selected, can be used to add notes etc. */
-        }
-      }
-      else {
-
-        self.props.callBack();
-        
         // reset previous
-        if (self.clickedObjectId != null) {
+        if (this.clickedObjectId != null) {
 
-          const exist = scene.getObjectById(self.clickedObjectId, true);
+          const exist = this.scene.getObjectById(this.clickedObjectId, true);
 
           exist.material.emissive.setHex(exist.currentHex);
         }
 
-        self.clickedObjectId = null;
+        this.clickedObjectId = obj.id;
       }
+
+      if (this.objectSelection === 0) {
+
+        intersects[0].object.material.transparent = true;
+        if (intersects[0].object.material.opacity < 1) {
+
+          intersects[0].object.material.opacity = 1;
+        }
+        else {
+
+          intersects[0].object.material.opacity = 0.3;
+        }
+      }
+      else if (this.objectSelection === 1) {
+
+        /* Object is selected, can be used to add notes etc. */
+      }
+    }
+    else {
+
+      this.props.callBack();
+
+      // reset previous
+      if (this.clickedObjectId != null) {
+
+        const exist = this.scene.getObjectById(this.clickedObjectId, true);
+
+        exist.material.emissive.setHex(exist.currentHex);
+      }
+
+      this.clickedObjectId = null;
     }
   }
 
