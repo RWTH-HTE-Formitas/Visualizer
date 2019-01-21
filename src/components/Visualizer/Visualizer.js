915e2eaa3aad484678a4222dccdb84373d434940
Arne Groskurth <arne.groskurth@gears9.de>
2019-01-21 15:24:14 +0100

    Merged methods

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -46,7 +46,35 @@ class Visualizer extends Component {
 
         if (objectName) {
 
-            this.fetch_object_data(objectName);
+            // create Array with all objects that have notes attached (in sample firebase: 3 ojects)
+            const jsonResults = this.getAnnotatedObjects();
+            if (!jsonResults.length) {
+
+                return;
+            }
+
+            const oData = jsonResults.find(x=> x.ID === objectName);
+            if (!oData || !oData.Status) {
+
+                this.setState({
+                showWindow: false
+                });
+
+                return;
+            }
+
+            this.setState({
+                showWindow: true,
+                objectData: oData,
+                camera: {
+                    pX: oData.Status.CameraPosition.x,
+                    pY: oData.Status.CameraPosition.y,
+                    pZ: oData.Status.CameraPosition.z,
+                    rX: oData.Status.CameraRotation.x,
+                    rY: oData.Status.CameraRotation.y,
+                    rZ: oData.Status.CameraRotation.z,
+                }
+            });
         }
 
         else {
@@ -91,40 +119,6 @@ class Visualizer extends Component {
         this.setState({
             defects: data
         });
-    }
-
-    fetch_object_data(objectName) {
-
-        // create Array with all objects that have notes attached (in sample firebase: 3 ojects)
-        const jsonResults = this.getAnnotatedObjects();
-        if (!jsonResults.length) {
-
-            return;
-        }
-
-        const oData = jsonResults.find(x=> x.ID === objectName);
-        if (!oData || !oData.Status) {
-
-            this.setState({
-                showWindow: false
-            });
-
-            return;
-        }
-
-        this.setState({
-            showWindow: true,
-            objectData: oData,
-            camera: {
-                pX: oData.Status.CameraPosition.x,
-                pY: oData.Status.CameraPosition.y,
-                pZ: oData.Status.CameraPosition.z,
-                rX: oData.Status.CameraRotation.x,
-                rY: oData.Status.CameraRotation.y,
-                rZ: oData.Status.CameraRotation.z,
-            }
-        });
-
     }
 
     render() {
