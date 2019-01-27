1ad91799f261f38140103004d04bd8e1c6ec173e
Achmad Rizky Haqiqi <rizky.haqiqi@rwth-aachen.de>
2019-01-27 09:13:33 +0100

    applying material ui card and table to mock ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Visualizer from "./components/Visualizer/Visualizer";
 import firebase from "./components/Firebase/Firebase.js";
+import { Table, TableHead, TableBody, TableRow, TableCell, TableFooter, Card, CardContent, Typography, Button } from "@material-ui/core";
 
 class App extends Component {
 
@@ -30,62 +31,52 @@ class App extends Component {
   render() {
 
     return (
-      <div style={{width: '1000px', margin: '1em auto'}}>
+      <div style={{ width: '1000px', margin: '1em auto' }}>
         <div className="container">
-          <div className="row">
-            <div className="col-xs-12" style={{width: '100%'}}>
-              <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                  <li className="breadcrumb-item"><a href="/">Formitas</a></li>
-                  <li className="breadcrumb-item"><a href="/">Project ABC</a></li>
-                  <li className="breadcrumb-item active" aria-current="page">Defect Notes</li>
-                </ol>
-              </nav>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12">
-              <h1 style={{margin: "1em 0 1em 0"}}>Project ABC - Defect Notes</h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12">
+            <div style={{ margin: '14px' }}>
               <Visualizer ref={element => { this._visualizer = element; }} database={firebase.database()} />
             </div>
+            <div style={{ margin: '14px' }}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" id="tableTitle">
+                    Table of objects
+                  </Typography>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Object ID</TableCell>
+                        <TableCell>Category</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {
+                        this.state.objects.map((data, i) => {
+                          return (
+                            <TableRow key={i}>
+                              <TableCell component="th" scope="row">{data.ID}</TableCell>
+                              <TableCell>{data.Category}</TableCell>
+                              <TableCell>{data.Name}</TableCell>
+                              <TableCell align="right">
+                                <Button onClick={() => { this._visualizer.selectObject(data.ID); }} variant="contained" size="small" color="primary">Show</Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })
+                      }
+                    </TableBody>
+                    <TableFooter>
+                      <TableRow>
+                        <TableCell>{this.state.objects.length} entries</TableCell>
+                      </TableRow>
+                    </TableFooter>
+                  </Table>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-          <div className="row" style={{margin: "2em 0"}}>
-            <table className="table table-striped">
-              <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">&nbsp;</th>
-              </tr>
-              </thead>
-              <tbody>
-              {
-                this.state.objects.map((data, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>{data.ID}</td>
-                      <td>{data.Name}</td>
-                      <td className="text-right"><button className="pull-right" onClick={() => {
-                        this._visualizer.selectObject(data.ID);
-                        return false;
-                      }}>Show</button></td>
-                    </tr>
-                  );
-                })
-              }
-              </tbody>
-              <tfoot>
-              <tr>
-                <td className="text-muted">{this.state.objects.length} entries</td>
-              </tr>
-              </tfoot>
-            </table>
-          </div>
-        </div>
       </div>
     );
   }
