e5455d5d9d72b8c44aff2754cd6411804393a32c
Achmad Rizky Haqiqi <rizky.haqiqi@rwth-aachen.de>
2019-01-27 09:34:46 +0100

    add formitas header, show more data to table, and text font improvement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import Visualizer from "./components/Visualizer/Visualizer";
 import firebase from "./components/Firebase/Firebase.js";
+import Moment from 'react-moment';
 import { Table, TableHead, TableBody, TableRow, TableCell, TableFooter, Card, CardContent, Typography, Button } from "@material-ui/core";
 
 class App extends Component {
@@ -31,7 +32,7 @@ class App extends Component {
   render() {
 
     return (
-      <div style={{ width: '1000px', margin: '1em auto' }}>
+      <div style={{ width: '1200px', margin: '1em auto' }}>
         <div className="container">
             <div style={{ margin: '14px' }}>
               <Visualizer ref={element => { this._visualizer = element; }} database={firebase.database()} />
@@ -45,9 +46,10 @@ class App extends Component {
                   <Table>
                     <TableHead>
                       <TableRow>
-                        <TableCell>Object ID</TableCell>
-                        <TableCell>Category</TableCell>
-                        <TableCell>Name</TableCell>
+                        <TableCell><Typography variant="body1">Object ID</Typography></TableCell>
+                        <TableCell><Typography variant="body1">Category</Typography></TableCell>
+                        <TableCell><Typography variant="body1">Name</Typography></TableCell>
+                        <TableCell><Typography variant="body1">Last Change</Typography></TableCell>
                         <TableCell></TableCell>
                       </TableRow>
                     </TableHead>
@@ -56,9 +58,15 @@ class App extends Component {
                         this.state.objects.map((data, i) => {
                           return (
                             <TableRow key={i}>
-                              <TableCell component="th" scope="row">{data.ID}</TableCell>
-                              <TableCell>{data.Category}</TableCell>
-                              <TableCell>{data.Name}</TableCell>
+                              <TableCell component="th" scope="row"><Typography variant="body1">{data.ID}</Typography></TableCell>
+                              <TableCell><Typography variant="body1">{data.Category}</Typography></TableCell>
+                              <TableCell><Typography variant="body1">{data.Name}</Typography></TableCell>
+                              <TableCell>
+                                <Typography variant="body1">
+                                  <Moment format="MMM DD, YYYY">{new Date(data.Status.Timestamp * 1000)}</Moment>
+                                  <b> <Moment format="HH:mm">{new Date(data.Status.Timestamp * 1000)}</Moment></b>
+                                </Typography>
+                              </TableCell>
                               <TableCell align="right">
                                 <Button onClick={() => { this._visualizer.selectObject(data.ID); }} variant="contained" size="small" color="primary">Show</Button>
                               </TableCell>
@@ -69,7 +77,7 @@ class App extends Component {
                     </TableBody>
                     <TableFooter>
                       <TableRow>
-                        <TableCell>{this.state.objects.length} entries</TableCell>
+                        <TableCell><Typography variant="body1">{this.state.objects.length} entries</Typography></TableCell>
                       </TableRow>
                     </TableFooter>
                   </Table>
