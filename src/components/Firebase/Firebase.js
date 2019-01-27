5e1428a902510f23baeee57a58e68dd9528d4787
Arne Groskurth <arne.groskurth@gears9.de>
2019-01-27 15:45:54 +0100

    Simplified code

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,16 +1,11 @@
-import firebase from 'firebase'
-//var firebase = require("firebase-admin");
-//var serviceAccount = require('./serviceAccountKey.json');
 
-// authenticiation 
-var config = {
+import firebase from 'firebase'
+
+export default firebase.initializeApp({
   apiKey: "AIzaSyAniO5TGmsQTWLvqnvlRNwlEjjiycYvevo",
   authDomain: "henmformitas.firebaseapp.com",
   databaseURL: "https://henmformitas.firebaseio.com",
   projectId: "henmformitas",
   storageBucket: "henmformitas.appspot.com",
   messagingSenderId: "505504123402"
-};
-
-firebase.initializeApp(config);
-export default firebase;
+});
